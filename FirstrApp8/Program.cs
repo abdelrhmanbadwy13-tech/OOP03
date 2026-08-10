22856ea2f93ad86cb72ae2b65b701231379f4021
abdelrhmanbadwy13 <abdelrhmanbadwy13@gmail.com>
2026-08-11 00:48:54 +0300

    part01

--- FirstrApp8/Program.cs
+++ FirstrApp8/Program.cs
@@ -8,22 +8,44 @@ namespace FirstrApp8
         static void Main(string[] args)
         {
             #region Part01
-            // Q1 Method Overloading:
+            //  Q1
+            // a) Method Overloading:
             // It is defining multiple methods with the same name but different parameters in the same class.
 
             //  Method Overriding:
             //It is redefining a method in a derived class that already exists in the base class using virtual and
 
-            //Q2 Static Binding:
+            //b) Static Binding:
             //The method to be executed is determined at compile time.
 
             //Dynamic Binding:
-                //The method to be executed is determined at runtime, depending on the actual object typ
+            //The method to be executed is determined at runtime, depending on the actual object typ
 
 
             #endregion
 
+            #region part01
+            // Q2
+            //a) 
+            //The sealed keyword prevents a class from being inherited
 
-}
+            // b) 
+            // A sealed class prevents the class from being inherited.
+            // A sealed method prevents a method from being overridden in derived classes.
+
+           //c)
+             //No, a sealed method cannot be overridden because the sealed keyword prevents further overriding of the method in derived classes.
+
+
+            #endregion
+        #region part2
+
+
+
+        #endregion
+
+
+
+        }
     }
 }
